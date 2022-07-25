7405f5d108d122d940bc71c632e0b6205a56b733
Natali <kovalnatali29@gmail.com>
2022-07-25 19:23:44 +0300

    Tuple in list

--- lesson_10_1.py
+++ lesson_10_1.py
@@ -1,9 +1,15 @@
-def add_stationery(list):
-    stationery = tuple(input("Введіть канцтовари: "))
-    return list.insert(0, stationery)
+"Створіть магазин канцтоварів використовуючи списки для зберігання елементів. Для додавання\
+елементів створіть функцію, яка буде запитувати дані в користувача і зібрані дані у вигляді\
+кортежу додавати у створений список на початку. Результат вивести на екран. Під час створення\
+дотримуйтесь правил специфікації PEP 8"
+
+
+def add_stationery(list_x):
+    stationery = input("Введіть канцтовари: ")
+    stationery_tuple = tuple(stationery.split())
+    return list_x.insert(0, stationery_tuple)
 
 
 list_stationery = ["олівець", "ручка", "ластик", "пенал"]
 add_stationery(list_stationery)
-
-
+print(list_stationery)
